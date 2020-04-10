6bbdb0facde71eddc9a07abb46784754b87ee840
Simon Prochazka <simon@fallion.net>
2020-04-10 15:38:40 +0200

    chore(generaterunner): break up into smaller pieces

---
+++ internal/generaterunner/generate_package.go
@@ -0,0 +1,31 @@
+package generaterunner
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/outillage/oto-tools/internal/npm"
+)
+
+// PackageJSON creates and saves the package.json file for uploading to npm
+func (runner *Runner) packageJSON(path string, packageOptions npm.Package) error {
+	npmPackage, err := npm.GeneratePackageInfo(packageOptions)
+
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(path, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path+"/package.json", npmPackage, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/generaterunner/generate_package_test.go
@@ -0,0 +1,31 @@
+package generaterunner
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/outillage/oto-tools/internal/npm"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePackage(t *testing.T) {
+	runner := &Runner{}
+
+	err := runner.packageJSON("./jstest", npm.Package{Name: "test-package", Version: "0.0.1"})
+
+	assert.NoError(t, err)
+
+	file, fileErr := ioutil.ReadFile("./jstest/package.json")
+
+	assert.NoError(t, fileErr)
+
+	generatedPackage := npm.Package{}
+
+	jsonErr := json.Unmarshal(file, &generatedPackage)
+
+	assert.NoError(t, jsonErr)
+
+	assert.Equal(t, "test-package", generatedPackage.Name)
+	assert.Equal(t, "0.0.1", generatedPackage.Version)
+}
