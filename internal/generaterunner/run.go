c45ef693491d5e9e40e9a1a647232cf39bba8187
Simon Prochazka <simon@fallion.net>
2020-04-10 15:38:40 +0200

    chore(generaterunner): break up into smaller pieces

--- internal/generaterunner/run.go
+++ internal/generaterunner/run.go
@@ -2,8 +2,6 @@ package generaterunner
 
 import (
 	"errors"
-	"io/ioutil"
-	"os"
 
 	"github.com/outillage/oto-tools/internal/npm"
 )
@@ -13,19 +11,7 @@ func (runner *Runner) Run(path string, packageOptions npm.Package) error {
 		return errors.New("path can't be empty")
 	}
 
-	npmPackage, err := npm.GeneratePackageInfo(packageOptions)
-
-	if err != nil {
-		return err
-	}
-
-	err = os.MkdirAll(path, 0777)
-
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(path+"/package.json", npmPackage, 0777)
+	err := runner.packageJSON(path, packageOptions)
 
 	if err != nil {
 		return err
--- internal/generaterunner/run_test.go
+++ internal/generaterunner/run_test.go
@@ -1,8 +1,6 @@
 package generaterunner
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"testing"
 
 	"github.com/outillage/oto-tools/internal/npm"
@@ -10,26 +8,9 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	runner := &Runner{}
+	runner := Runner{}
 
-	emptyPathErr := runner.Run("", npm.Package{})
+	pathErr := runner.Run("", npm.Package{})
 
-	assert.Error(t, emptyPathErr)
-
-	err := runner.Run("./jstest", npm.Package{Name: "test-package", Version: "0.0.1"})
-
-	assert.NoError(t, err)
-
-	file, fileErr := ioutil.ReadFile("./jstest/package.json")
-
-	assert.NoError(t, fileErr)
-
-	generatedPackage := npm.Package{}
-
-	jsonErr := json.Unmarshal(file, &generatedPackage)
-
-	assert.NoError(t, jsonErr)
-
-	assert.Equal(t, "test-package", generatedPackage.Name)
-	assert.Equal(t, "0.0.1", generatedPackage.Version)
+	assert.Error(t, pathErr)
 }
