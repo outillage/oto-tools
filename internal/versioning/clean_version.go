6144ed665e8f51129a53576b3b140063f7af6fe1
Simon Prochazka <simon@fallion.net>
2020-04-12 22:32:01 +0200

    fix: further fixes to version generation

--- internal/versioning/clean_version.go
+++ internal/versioning/clean_version.go
@@ -3,7 +3,7 @@ package versioning
 import "strings"
 
 func cleanVersion(version string) string {
-	version = strings.TrimPrefix(version, "/refs/tags/")
+	version = strings.TrimLeft(version, "refs/tag")
 
 	version = strings.TrimLeft(version, "v")
 
--- internal/versioning/clean_version_test.go
+++ internal/versioning/clean_version_test.go
@@ -9,6 +9,7 @@ import (
 func TestCleanVersion(t *testing.T) {
 	tests := map[string]string{
 		"/refs/tags/v0.0.1": "0.0.1",
+		"refs/tags/v0.0.1":  "0.0.1",
 		"v0.0.2":            "0.0.2",
 		"0.0.3":             "0.0.3",
 	}
