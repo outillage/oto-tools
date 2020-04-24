4544b2214bcfe64b267f90c7591e48c2311ec8dc
Simon Prochazka <simon@fallion.net>
2020-04-24 15:48:38 +0200

    chore: use magefiles templates

--- mage.go
+++ mage.go
@@ -3,21 +3,21 @@
 package main
 
 import (
-	"github.com/magefile/mage/sh"
+	"github.com/aevea/magefiles"
 )
 
 func Test() error {
-	return sh.RunV("go", "test", "./...", "-v", "-race")
+	return magefiles.Test()
 }
 
 func GoModTidy() error {
-	err := sh.RunV("go", "mod", "tidy", "-v")
-	if err != nil {
-		return err
-	}
-	return sh.RunV("git", "diff-index", "--quiet", "HEAD")
+	return magefiles.GoModTidy()
 }
 
 func Install() error {
-	return sh.RunV("go", "install", "github.com/pacedotdev/oto")
+	dependencies := []string{
+		"github.com/pacedotdev/oto",
+	}
+
+	return magefiles.Install(dependencies)
 }
